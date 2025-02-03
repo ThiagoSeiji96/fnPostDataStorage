76937da43c3a4f1496e2a01630468e6d0afe8424
Thiago Seiji Imai Rozner <thiago.rozner@viveo.com.br>
2025-02-03 00:16:26 -0300

    Adicionar arquivos de projeto.

---
+++ fnPostDataStorage/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.Configure<KestrelServerOptions>(opts =>
+{
+    opts.Limits.MaxRequestBodySize= 1024 * 1024 * 100; // 10MB
+});
+
+
+builder.Build().Run();
